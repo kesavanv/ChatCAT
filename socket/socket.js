d8a662343e956a6d0fd2c1f00955b0cd28e076b4
Varatharajan, Kesavan <kvaratharajan@paypal.com>
2015-08-01 17:46:04 +0530

    joining a new room

--- socket/socket.js
+++ socket/socket.js
@@ -9,4 +9,14 @@ module.exports = function (io, rooms) {
             socket.emit('roomupdate', JSON.stringify(rooms));
         });
     });
+
+    var messages = io.of('/messages').on('connection', function (socket) {
+        console.log('Connection established on the server !');
+
+        socket.on('joinroom', function (data) {
+            socket.username = data.user;
+            socket.userPic = data.userPic;
+            socket.join(data.room);
+        });
+    });
 };
