916a0d9971c91e3e5f4d2afc2144b7b38f0a4d64
Varatharajan, Kesavan <kvaratharajan@paypal.com>
2015-08-01 16:04:15 +0530

    Adding logout functionality and removing old code

--- routes/routes.js
+++ routes/routes.js
@@ -24,13 +24,9 @@ module.exports = function (express, app, passport) {
         res.render('chatrooms', {title: 'Chatrooms', user:req.user});
     });
 
-    router.get('/setcolor', function (req, res, next) {
-        req.session.favColor = "Red";
-        res.send('Setting fav color');
-    });
-
-    router.get('/getcolor', function (req, res, next) {
-        res.send('Fav color: ', (req.session.favColor === undefined ? "Not found": req.session.favColor));
+    router.get('/logout', function (req, res, next) {
+        req.logout();
+        res.redirect('/');
     });
 
     app.use('/', router);
